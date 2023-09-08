b02d7886be3bbb714e3ee58fb90e132c80acf5a2
Ondřej Műller <devcoft@gmail.com>
2023-09-08 22:47:32 +0200

    project init

---
+++ Signals Unity project/Assets/_Package/Editor/AssetCollectionData.cs
@@ -0,0 +1,37 @@
+﻿// using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Coft.AssetCollection
+{
+    public class AssetCollectionData : ScriptableObject
+    {
+        // [HideInInlineEditors]
+        public AssetCollection Collection;
+        // [InlineEditor]
+        public List<Object> Assets;
+
+#if UNITY_EDITOR
+        // [HideInInlineEditors]
+        // [Button(nameof(Regenerate))]
+        public void Regenerate()
+        {
+            var path = AssetDatabase.GetAssetPath(this);
+            var collection =
+                AssetDatabase.FindAssets(
+                        Collection.SearchQuery,
+                        new[] { Path.GetDirectoryName(path) }
+                    )
+                    .Select(guid => AssetDatabase.LoadAssetAtPath<Object>(AssetDatabase.GUIDToAssetPath(guid)))
+                    .Where(obj => obj != this && obj != Collection)
+                    .ToList();
+            Assets = collection;
+            AssetDatabase.RemoveObjectFromAsset(this);
+            AssetDatabase.CreateAsset(this, path);
+        }
+#endif
+    }
+}
