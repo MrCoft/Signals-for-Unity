30311ba927cbd5a4f23b81a672674d5d4609cb2a
Ondřej Műller <devcoft@gmail.com>
2023-10-15 00:02:42 +0200

    NuGet build works

--- Build scripts solution/PublishNuget/PublishNuget.cs
+++ Build scripts solution/PublishNuget/PublishNuget.cs
@@ -1,71 +1,55 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
+using CliWrap;
 
 Console.WriteLine("Publishing NuGet package...");
+Console.WriteLine(Assembly.GetEntryAssembly()!.Location);
 
-var packageJson = File.ReadAllText(Path.Join(Application.dataPath, "Signals/package.json"));
+var solutionDir = Path.GetFullPath(Path.Combine(
+    Assembly.GetEntryAssembly()!.Location,
+    "..", "..", "..", "..", ".."));
+
+var packageJson = File.ReadAllText(Path.Join(solutionDir, "../Signals Unity project/Assets/Signals/package.json"));
 var version = Regex.Match(packageJson, "\"version\": \"(.+?)\"").Groups[1].Value;
 Console.WriteLine("Version: " + version);
 
-var repoRoot = Path.Join(Application.dataPath, "../..");
+var repoRoot = Path.Join(solutionDir, "..");
 Directory.SetCurrentDirectory(repoRoot);
 
 var envContent = File.ReadAllText(".env");
 var envLine = Regex.Split(envContent, "\r\n|\r|\n").FirstOrDefault(line => line.StartsWith("NUGET_API_KEY"));
 var nugetApiKey = envLine.Split("=")[1].Trim();
-Console.WriteLine($"NuGet API Key loaded: {nugetApiKey.Substring(0, 2)}..");
+Console.WriteLine($"NuGet API Key loaded: {nugetApiKey.Substring(0, 4)}...{nugetApiKey.Substring(nugetApiKey.Length - 4)}");
 
-Directory.SetCurrentDirectory("Signals NuGet project");
+Directory.SetCurrentDirectory("Build scripts solution/NugetProject");
 var csproj = File.ReadAllText("Coft.Signals.csproj");
 csproj = Regex.Replace(csproj, "<Version>(.+?)</Version>", $"<Version>{version}</Version>");
 Console.WriteLine("Updated version in Coft.Signals.csproj");
 File.WriteAllText("Coft.Signals.csproj", csproj);
 
 Console.WriteLine("Running dotnet build...");
-var startInfo = new ProcessStartInfo()
+await using var stdOut = Console.OpenStandardOutput();
+await using var stdErr = Console.OpenStandardError();
+await (Cli.Wrap("dotnet").WithArguments(new[]
 {
-    FileName = "dotnet",
-    UseShellExecute = false,
-    RedirectStandardError = true,
-    RedirectStandardInput = true,
-    RedirectStandardOutput = true,
-    CreateNoWindow = true,
-    Arguments = "build"
-};
-Process myProcess = new Process
+    "build",
+    "--configuration",
+    "Release",
+}) | (stdOut, stdErr)).ExecuteAsync();
+
+Directory.SetCurrentDirectory("bin/release");
+await (Cli.Wrap("dotnet").WithArguments(new[]
 {
-    StartInfo = startInfo
-};
-myProcess.Start();
-myProcess.WaitForExit();
-var output = myProcess.StandardOutput.ReadToEnd();
-Console.WriteLine(output);
-
-        
-        // dotnet build
-        // go to bin/release
-        // pick Coft.Signals.<v>.nupkg and snupkg
-        // dotnet nuget push Contoso.08.28.22.001.Test.1.0.0.nupkg --api-key qz2jga8pl3dvn2akksyquwcs9ygggg4exypy3bhxy6w6x6 --source https://api.nuget.org/v3/index.json
-        // read API key from .env file
-        // nuget SetApiKey Your-API-Key
-        // dotnet nuget push .snupkg
-
-        // string targetDir = $"{TargetDir}/{AppName} Windows/{AppName}.exe";
-        // GenericBuild(Scenes, targetDir, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, BuildOptions.StrictMode);
-// private string ExecuteProcessTerminal(string argument)
-// {
-//     try
-//     {
-//         
-//  
-//         return output;
-//     }
-//     catch (Exception e)
-//     {
-//         print(e);
-//         return null;
-//     }
-// }
+    "nuget",
+    "push",
+    $"Coft.Signals.{version}.nupkg",
+    "--api-key",
+    nugetApiKey,
+    "--source",
+    "https://api.nuget.org/v3/index.json",
+    "--skip-duplicate",
+}) | (stdOut, stdErr)).ExecuteAsync();
+// NOTE: this also pushes .snupkg
